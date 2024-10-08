2756c72111e4b295111af0e6c2938ccd740637fa
vishvajeet shukla <djritikshukla@gmail.com>
2024-10-08 14:56:37 +0530

    imporve nav bar ui

--- src/Comp/Navbar.jsx
+++ src/Comp/Navbar.jsx
@@ -1,21 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../css/Navbar.css';
 
 const pages = [
   { name: 'Home', link: '/' }, 
   { name: 'Product', link: '/Product' },
-  { name: 'City', link: '/city' }
+  { name: 'City', link: '/city' },
+  { name: 'Food', link: '/Food' },
+  { name: 'About', link: '/about' },
+  { name: 'Contact', link: '/Contact' }
 ];
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="container">
         <div className="logo">
-          <Link to="/">LOGO</Link>
+          <Link to="/">India Tourism</Link>
         </div>
-        <div className="nav-links">
+        <div className="hamburger" onClick={toggleMenu}>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+        <div className={`nav-links ${isOpen ? 'active' : ''}`}>
           {pages.map((page) => (
             <Link key={page.name} to={page.link}>
               {page.name}
