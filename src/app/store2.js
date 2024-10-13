edd77ff7a7e43da85e242f6f245e77cda570c296
vishvajeet shukla <djritikshukla@gmail.com>
2024-10-13 14:43:44 +0530

    remove redux from project due to image import problem

--- src/app/store2.js
+++
@@ -1,8 +0,0 @@
-import dataReducer from "../features/tourismRed/tourims";
-import { configureStore } from "@reduxjs/toolkit";
-
-export default configureStore({
-    reducer:{
-      data: dataReducer
-    }
-})
